1f4401c7e58156ea4d5c5434ccdf3e1f63280e76
NaranLidzhiev <lidzhiev-03@mail.ru>
2021-11-09 01:24:26 +0300

    косвенный доступ

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-    char *char_pointer;
-    char letter = 'A';
-    char_pointer = &letter;
-    cout << *char_pointer << endl;
-    //можно присвоить переменной letter новое значение, используя указатель
-    *char_pointer = 'B';
-    cout << letter;
+  int *int_pointer = 0;
+  int intVar = 5;
+  int_pointer = &intVar;
+  //указатель получает адрес переменной intVar
+  // теперь int_pointer указывает на  intVar
+  *int_pointer = 1;
+  // переменной, на которую указывает int_pointer,  будет присвоение значение 1
+  // При этом только содержимое intVar равно 1
+  intVar = *int_pointer + 1;
+  //значение intVar будет изменено на сумму 1 и значение, на
+  // которое указывает  int_pointer. Но только сама перменная.
+  // Поэтому значение intVar в итоге будет равно 2
 }
